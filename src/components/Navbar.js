3f7a0cc16c785da52fe888bb73ba5c09e9c8a955
Rez Bst <153891357+Marshalcoder@users.noreply.github.com>
2024-03-23 10:13:31 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,24 +11,25 @@ const Navbar = () => {
             <div></div>
             {/* Middle */}
             <div className="flex items-center relative">
-                <div className="absolute inset rounded-full mr-0.2 ">
-                    <IoSearchCircle className="text-transparent-100 h-10 w-10" />
+                <div className="absolute inset-3 right-1 inset-y-0 left-0.5 flex items-center text-gray-400">
+                    <IoSearchCircle className="text-black hover:text-gray-800 h-10 w-10" />
+                    <span className="ml-2">Search (e.g., address, zip...)</span>
                 </div>
 
-                <input type="text" placeholder="Search (e.g., address, zip...)" className="bg-gray-200 text-gray-700 placeholder-opacity-50 rounded-full pl-10 pr-30 py-2 shadow-inner focus:outline-none focus:ring focus:border-yellow-300 w-full" />
-                <button className=" absolute right-2 bg-yellow-400 hover:bg-yellow-600 text-black px-2 py-2 rounded-full shadow-md">Find Chargers Near Me</button>
+                <input type="text" placeholder="" className="bg-gray-200 text-gray-700 placeholder-opacity-100 rounded-full pl-12 pr-20 py-2 shadow-inner focus:outline-none focus:ring focus:border-yellow-300 w-96" />
+                <button className="absolute right-0.5 bg-yellow-400 hover:bg-yellow-600 text-black px-2 py-2 rounded-full shadow-md">Find Charger Near Me</button>
             
             </div>
             {/* Right */}
             <div className="flex items-center pr-3">
                 <button className="bg-yellow-400 hover:bg-yellow-600 text-black px-4 py-2 rounded-full shadow-md">Register My Charger</button>
                 <div className="bg-black rounded-full p-5 mr-2" >
-                    <FaMapSigns className="ml-4 text-white mr-2" />
+                    <FaMapSigns className="ml-4 text-white mr-2 hover:text-slate-700" />
                 </div>
                 <div className="ml-30 bg-black rounded-full p-1 border-white border">
                     <div className="flex items-center">
-                        <ImMenu className="text-white h-50" />
-                        <FaUserCircle className="ml-4 text-white h-50 " />
+                        <ImMenu className="text-white h-50 hover:text-gray-700" />
+                        <FaUserCircle className="ml-4 text-white h-50 hover:text-slate-600 " />
                     </div>
                 </div>
                 <div className="ml-4 flex items-center bg-black rounded-full p-2">
